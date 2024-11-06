704f1b5f7c7c0eece91ec296042f1ddd354c938e
José Anderson da Silva <joseandersonsilv4@gmail.com>
2024-11-06 20:13:31 -0300

    chore(media-service): merge

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import love from 'eslint-config-love'
+
+export default [
+  {
+    ...love,
+    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      ...love.rules,
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-magic-numbers": "off",
+    },
+  },
+]
