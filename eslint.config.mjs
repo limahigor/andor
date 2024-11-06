b67aac6a1b5e031268e49702b870e0d4151aab73
Higor <limahigor.g@gmail.com>
2024-11-05 21:40:39 -0300

    chore: add jest configs and scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default [
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-magic-numbers": "off",
+      "eslint-comments/require-description": "off",
       "@typescript-eslint/no-unsafe-assignment": "off"
     },
   },
---
+++ packages/auth-service/jest-integration-config.js
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const config = require("./jest.config");
+
+config.testMatch = ['**/*.test.ts']
+module.exports = config
