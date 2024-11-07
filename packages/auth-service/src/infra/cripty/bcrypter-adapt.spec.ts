bf4fc0ebd0c3358a5ea343cab77b71edfcb25060
Higor <limahigor.g@gmail.com>
2024-11-07 20:15:12 -0300

    test: ensure BcryptAdapter throws if bcrypt throw

--- packages/auth-service/src/infra/cripty/bcrypter-adapt.spec.ts
+++ packages/auth-service/src/infra/cripty/bcrypter-adapt.spec.ts
@@ -15,19 +15,25 @@ const makeSut = (): BcryptAdapter => {
 describe('BCrypt Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
     const sut = makeSut()
-    
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     
-    await sut.encrypt('any_valye')
+    await sut.encrypt('any_value')
   
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('Should return a hash on succes', async () => {
     const sut = makeSut()
-
-    const hash = await sut.encrypt('any_valye')
+    const hash = await sut.encrypt('any_value')
 
     expect(hash).toBe('hash')
   })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut();
+
+    (bcrypt.hash as jest.Mock).mockRejectedValueOnce(new Error());
+
+    await expect(sut.encrypt('any_value')).rejects.toThrow();
+  });
 })
