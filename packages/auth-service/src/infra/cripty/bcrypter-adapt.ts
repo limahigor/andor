c4bc61746b03be5a7a317caf14d3141cafa7e174
Higor <limahigor.g@gmail.com>
2024-11-07 20:14:58 -0300

    feat: ensure BcryptAdapter throws if bcrypt throw

--- packages/auth-service/src/infra/cripty/bcrypter-adapt.ts
+++ packages/auth-service/src/infra/cripty/bcrypter-adapt.ts
@@ -9,8 +9,7 @@ export class BcryptAdapter implements Encrypter{
   }
 
   async encrypt(value: string): Promise<string>{
-    const hash = await bcrypt.hash('any_value', 12)
-
+    const hash = await bcrypt.hash(value, this.salt)
     return hash
   }
 } 
