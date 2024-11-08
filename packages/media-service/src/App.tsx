16fd2089ae1689138ad44f1181c1c471cc69eeff
José Anderson da Silva <joseandersonsilv4@gmail.com>
2024-11-08 02:22:41 -0300

    chore(media-service): implement media modal and backend server setup

--- packages/media-service/src/App.tsx
+++ packages/media-service/src/App.tsx
@@ -4,11 +4,11 @@ import Navbar from './components/novbar/Navbar';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <Home />
       {/* Outros componentes podem ser adicionados aqui */}
-    </div>
+    </>
   );
 };
 
--- packages/media-service/src/components/home/home.tsx
+++ packages/media-service/src/components/home/home.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css'; // Certifique-se de estilizar conforme necessário
+import './Home.css';
 import { FaPlus } from 'react-icons/fa'; // Ícone de adicionar
 
 const Home: React.FC = () => {
+  const [showMediaModal, setShowMediaModal] = useState(false);
+
+  const handleShowMediaModal = () => setShowMediaModal(true);
+  const handleCloseMediaModal = () => setShowMediaModal(false);
+
   return (
     <div className="container mt-4">
       <section>
         <div className="d-flex justify-content-between align-items-center mb-3">
           <h2>Últimas Mídias</h2>
-          <button className="btn btn-success d-flex align-items-center">
+          <button className="open-modal-button" onClick={handleShowMediaModal}>
             <FaPlus className="mr-2" /> Adicionar Mídia
           </button>
+          
         </div>
         <div className="row">
           {[1, 2, 3, 4, 5, 6].map((num) => (
@@ -50,6 +56,51 @@ const Home: React.FC = () => {
           ))}
         </div>
       </section>
+        
+      {showMediaModal && (
+        <div className="modal-overlay">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Adicionar Nova Mídia</h5>
+                <button type="button" className="btn-close" onClick={handleCloseMediaModal}></button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="mb-3">
+                    <label htmlFor="mediaTitle" className="form-label">Título da Mídia</label>
+                    <input type="text" className="form-control" id="mediaTitle" placeholder="Digite o título da mídia" />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="mediaType" className="form-label">Canal</label>
+                    <select className="form-select" id="mediaType">
+                      <option value="IPTV">Nenhum</option>
+                      <option value="Torrent">Canal 1</option>
+                      <option value="Google Drive">Canal 2</option>
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="mediaType" className="form-label">Tipo</label>
+                    <select className="form-select" id="mediaType">
+                      <option value="IPTV">IPTV</option>
+                      <option value="Torrent">Torrent</option>
+                      <option value="Google Drive">Google Drive</option>
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="mediaLink" className="form-label">Link</label>
+                    <input type="text" className="form-control" id="mediaLink" placeholder="Digite o link da mídia" />
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={handleCloseMediaModal}>Cancelar</button>
+                <button type="button" className="btn btn-primary">Salvar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
