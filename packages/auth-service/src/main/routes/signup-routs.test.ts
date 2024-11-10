c5813869d2011a8bcb519c817f6093400b8bae8e
Higor <limahigor.g@gmail.com>
2024-11-10 01:32:23 -0300

    refactor: conect to mongodb before test

--- packages/auth-service/src/main/routes/signup-routs.test.ts
+++ packages/auth-service/src/main/routes/signup-routs.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest';
 import app from '../config/app';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 
 describe('SignUp Routes', () => {
+
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__ as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return true on success', async () => {
     app.get('/test_cors', (req, res) => {
       res.send();
