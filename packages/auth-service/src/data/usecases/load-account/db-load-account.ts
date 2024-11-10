7341d75de5fcd1f2c0d5132fb0f2f483e411fd6a
Higor <limahigor.g@gmail.com>
2024-11-10 20:29:48 -0300

    test: should return a void string if CheckAccountByUsernameRepository returns true

--- packages/auth-service/src/data/usecases/load-account/db-load-account.ts
+++ packages/auth-service/src/data/usecases/load-account/db-load-account.ts
@@ -1,24 +1,28 @@
 import type { CheckAccountByUsernameRepository, Encrypter, Hasher, LoadAccount, LoadAccountModel, LoadAccountRepository } from "./db-load-account-protocols";
 
-export class DbLoadAccount implements LoadAccount{
-    private readonly hasher: Hasher
-    private readonly encrypter: Encrypter
-    private readonly loadAccountRepository: LoadAccountRepository
-    private readonly checkAccountByUsernameRepository: CheckAccountByUsernameRepository
-    
-    constructor(hasher: Hasher, encrypter: Encrypter, loadAccountRepository: LoadAccountRepository, checkAccountByUsernameRepository: CheckAccountByUsernameRepository) {
-        this.hasher = hasher
-        this.encrypter = encrypter
-        this.loadAccountRepository = loadAccountRepository
-        this.checkAccountByUsernameRepository = checkAccountByUsernameRepository
+export class DbLoadAccount implements LoadAccount {
+  private readonly hasher: Hasher
+  private readonly encrypter: Encrypter
+  private readonly loadAccountRepository: LoadAccountRepository
+  private readonly checkAccountByUsernameRepository: CheckAccountByUsernameRepository
+
+  constructor(hasher: Hasher, encrypter: Encrypter, loadAccountRepository: LoadAccountRepository, checkAccountByUsernameRepository: CheckAccountByUsernameRepository) {
+    this.hasher = hasher
+    this.encrypter = encrypter
+    this.loadAccountRepository = loadAccountRepository
+    this.checkAccountByUsernameRepository = checkAccountByUsernameRepository
+  }
+
+
+  async login(loginData: LoadAccountModel.Params): Promise<LoadAccountModel.Result> {
+    let status = ''
+    const exists = await this.checkAccountByUsernameRepository.checkByUsername(loginData.username)
+
+    if(!exists){
+      const hashedPassword = await this.hasher.hasher(loginData.password)
+      status = await this.loadAccountRepository.login({ ...loginData, password: hashedPassword })
     }
-    
 
-    async login(loginData: LoadAccountModel.Params): Promise<LoadAccountModel.Result>{
-        const hashedPassword = await this.hasher.hasher(loginData.password)
-
-        await this.loadAccountRepository.login({ ...loginData, password: hashedPassword })
-
-        return ''
-    }
+    return status
+  }
 }
