81e38e48471ea4baff5b4ca0e737035a14dea9f7
Anderson Silva <joseandersonsil4@gmail.com>
2024-11-23 03:27:13 -0300

    feat: atualiza estrutura do projeto e configurações do package.json

--- packages/media-service/backend/src/database.ts
+++ packages/media-service/backend/src/database.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectToDatabase = async (): Promise<void> => {
   try {
-    const MONGO_URI = "mongodb+srv://anderson:masterkey@media-service.385ds.mongodb.net/?retryWrites=true&w=majority&appName=media-service";
+    // const MONGO_URI = "mongodb+srv://anderson:masterkey@media-service.385ds.mongodb.net/?retryWrites=true&w=majority&appName=media-service";
+    const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/media-service';
     await mongoose.connect(MONGO_URI, {
     });
     console.log('Conectado ao MongoDB');
